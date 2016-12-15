5ac33e4e8fd30adc09f4b234485098bacd8d62f4
fizashahamatkhan <fiza.shahmat@thecodeitcompany.com>
2016-12-15 10:13:36 -0800

    Shortcode 1 Author name syntax added

--- shortcodes/bep_shortodes_1/bep_shortcode_template.php
+++ shortcodes/bep_shortodes_1/bep_shortcode_template.php
@@ -23,7 +23,9 @@ $return_string.='	</h3>';
 $return_string.='	<div class="bep-module-meta-info">';
 $return_string.='		<span class="bep-post-author-name">';
 $return_string.='			<a href="'. get_author_posts_url( get_the_ID() ) .'">';
-$return_string.='				'. get_the_author_meta( 'display_name', get_the_ID() ) . '';
+
+/*$return_string.='				'. get_the_author_meta( 'display_name', get_the_ID() ) . '';*/
+$return_string.=''.get_the_author() .'';
 $return_string.='			</a>';
 $return_string.='			<span>-</span>';
 $return_string.='		</span>';
