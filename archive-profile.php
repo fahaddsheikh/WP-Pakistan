8af971cddc86e7e903ace2181ea8db5670b6df7f
Fahad Sheikh <fahad.sheikh@thecodeitcompany.com>
2016-12-28 12:36:19 +0500

    Added Radius functionality to bep archive search and moved the archive template to tha parent directory.

--- archive-profile.php
+++ archive-profile.php
@@ -1,3 +1,3 @@
 <?php get_header(); ?>
-<?php get_template_part( 'profile-module/templates/be_profile', 'archive' ); ?>
+<?php get_template_part( 'bep', 'archive' ); ?>
 <?php get_footer(); ?>
--- bep-custom-searchform.php
+++ bep-custom-searchform.php
@@ -33,7 +33,25 @@ function bep_categories_out($categories, $post_type) {
 	        elseif ($current_posttype['post_type'] == 'ait-item') {
 	        	bep_categories_out('ait-items', $current_posttype);
 	        	bep_categories_out('ait-locations', $current_posttype);
-       			echo "<input type='hidden' name='a' value='true'>";
+       			echo "<input type='hidden' name='a' value='true'>"; 
+       			echo "<div class='radius '>";
+				echo 	"<div class='radius-toggle radius-input-visible'>Radius: Off</div>";
+				echo 		"<input type='hidden' name='lat' value='' id='latitude-search' class='latitude-search' disabled=''>";
+				echo 		"<input type='hidden' name='lon' value='' id='longitude-search' class='longitude-search' disabled=''>";
+				echo 		"<input type='hidden' name='runits' value='km' disabled=''>";
+				echo 		"<input type='hidden' name='runits' value='km' disabled=''>";
+				echo	"<div class='radius-display radius-input-hidden'>";
+				echo 		"<span class='radius-clear'><i class='fa fa-times'></i></span>";
+				echo 		"<span class='radius-text'>Radius:</span>";
+				echo 	"</div>";
+				echo 	"<div class='radius-popup-container radius-input-hidden'>";
+				echo 		"<span class='radius-popup-close'><i class='fa fa-times'></i></span>";
+				echo 		"<span class='radius-value'></span>";
+				echo 		"<span class='radius-units'>km</span>";
+				echo 		"<input type='range' name='rad' class='radius-search' value='0.1' min='0.1' step='0.1' max='100' disabled=''>";
+				echo 		"<span class='radius-popup-help'>Set radius for geolocation</span>";
+				echo 	"</div>";
+				echo  "</div>";
 	        } 
         ?>
         <input class="screen-reader-text" type="submit" id="search-submit" value="Search" />
