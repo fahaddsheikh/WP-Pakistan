fe44d4a3ba292907bd54f306c097b8a59a7d3e56
Fahad Sheikh <fahad.sheikh@thecodeitcompany.com>
2017-01-05 17:15:21 +0500

    Fixed the auto-detect functionality to box types in shortcodes

--- shortcodes/bep_biggrid/bep_big_grid_square_small_two_template_4.php
+++ shortcodes/bep_biggrid/bep_big_grid_square_small_two_template_4.php
@@ -8,10 +8,23 @@
 	$return_string .=		"<div class='{$prefix}meta-info-container'>	";
 	$return_string .=			"<div class='{$prefix}meta-align'>	";
 	$return_string .=				"<div class='{$prefix}big-grid-meta'>	";
-										$bep_selectedtype = get_post_meta( get_the_id(), 'bep_type', true );
+									$bep_selectedtype = get_post_type();
 										if (isset($bep_selectedtype) && !empty($bep_selectedtype) ) {
-	$return_string .=						"<span class='{$prefix}post-category'>" . $bep_selectedtype . "</span>";
-										} 
+	$return_string.='						<span class="bep_post-category">';
+											if ($bep_selectedtype == 'ait-item') {
+	$return_string.='							Business';
+											}
+											elseif ($bep_selectedtype == 'post') {
+	$return_string.='							Blog';
+											}
+											elseif ($bep_selectedtype == 'ait-event-pro') {
+	$return_string.='							Event';
+											}
+											elseif ($bep_selectedtype == 'profile') {
+	$return_string.='							Profile';
+											}
+	$return_string.='						</span>';
+										}
 	$return_string .=					"<h3 class='entry-title {$prefix}module-title'>";
 	$return_string .=						"<a href='" . get_permalink() . "' rel='bookmark' title='" . get_the_title() . "'>" . get_the_title() . "</a>	";
 	$return_string .=					"</h3>";
